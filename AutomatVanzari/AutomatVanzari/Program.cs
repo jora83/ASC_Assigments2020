594648f9c5daa1489e8372d865df4cfc7db9b931
alex jora <joraalex13@gmail.com>
2020-11-20 16:03:50 +0200

    Automat de vanzari

---
+++ AutomatVanzari/AutomatVanzari/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace AutomatVanzari
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Automat();
+            }
+        }
+
+        static void Automat()
+        {
+            int monede = 0, rest = 0, suma = 0;
+
+            Console.WriteLine("Introduceti monede de 5 10 sau 25 de centi in aparat");
+
+            while (suma < 20)
+            {
+
+                monede = int.Parse(Console.ReadLine());
+                if (monede == 5 || monede == 10 || monede == 25)
+                {
+                    suma += monede;
+                }
+                else
+                {
+                    Console.WriteLine("Moneda introdusa nu este corecta");
+                }
+                Console.WriteLine($"Aveti {suma} centi in aparat");
+
+            }
+
+
+            Console.WriteLine("Produsul a fost servit");
+            if (suma > 20)
+            {
+                rest = suma - 20;
+            }
+            Console.WriteLine($"rest: {rest} centi");
+            Console.WriteLine(" ");
+
+        }
+    }
+}
